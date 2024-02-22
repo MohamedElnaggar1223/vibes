a58e12c7548ea025e958c4be5b84e8a77da31f29
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-02-22 13:37:20 +0200

    Animate Presence

---
+++ app/[index]/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { events } from "@/constants"
+import { AnimatePresence, motion } from "framer-motion"
+import Image from "next/image"
+import { useParams } from "next/navigation"
+
+export default function EventPage() 
+{
+    const { index } = useParams()
+    const selectedEvent = events[parseInt(index.toString())]
+
+    return (
+        <AnimatePresence>
+            <section className='flex w-full'>
+                <motion.div layoutId={index.toString()} className='w-[500px] p-5'>
+                    <Image
+                        src={selectedEvent.imgUrl}
+                        width={500}
+                        height={500}
+                        alt={selectedEvent.title} 
+                    />
+                </motion.div>
+                <div className='flex-1'>
+                    Hello
+                </div>
+            </section>
+        </AnimatePresence>
+    )
+}
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const events = [
+    {
+        title: 'Majid al-Muhandis',
+        price: '3,200 EGP',
+        imgUrl: '/assets/secondEvent.jfif'
+    },
+    {
+        title: 'Al Hilal VS Al Nassr',
+        price: '2,900 EGP',
+        imgUrl: '/assets/firstevent.jfif'
+    },
+    {
+        title: 'Al Hilal VS Al Nassr',
+        price: '2,900 EGP',
+        imgUrl: '/assets/thirdEvent.png'
+    },
+    {
+        title: 'Al Hilal VS Al Nassr',
+        price: '2,900 EGP',
+        imgUrl: '/assets/fourthEvent.jfif'
+    },
+    {
+        title: 'Al Hilal VS Al Nassr',
+        price: '2,900 EGP',
+        imgUrl: '/assets/fourthEvent.jfif'
+    },
+]
