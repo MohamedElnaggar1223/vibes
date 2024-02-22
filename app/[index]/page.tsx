481e0586cdd76f37395b72cc861b0f564c3ed485
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-02-22 21:48:01 +0200

    Event Card

--- app/[index]/page.tsx
+++ app/[index]/page.tsx
@@ -26,13 +26,13 @@ export default function EventPage()
                     <div className='flex flex-col p-3 gap-4'>
                         <p className='font-poppins text-2xl font-bold text-white'>{selectedEvent.title}</p>
                         <div className='w-full flex justify-between items-center'>
-                            <p className='font-poppins text-sm font-light text-white'>Al Manara Arena</p>
-                            <p className='font-poppins text-sm font-light text-white mr-4'>Cairo, Egypt</p>
+                            <p className='font-poppins text-sm font-extralight text-white'>Al Manara Arena</p>
+                            <p className='font-poppins text-sm font-extralight text-white mr-4'>Cairo, Egypt</p>
                         </div>
-                        <p className='font-poppins text-sm font-light text-white'>August, 1, 2024 | 8:30 PM GMT</p>
-                        <p className='font-poppins text-sm font-light text-white'>Gates open 4:30PM | Gates close 7:00 PM</p>
+                        <p className='font-poppins text-sm font-extralight text-white'>August, 1, 2024 | 8:30 PM GMT</p>
+                        <p className='font-poppins text-sm font-extralight text-white'>Gates open 4:30PM | Gates close 7:00 PM</p>
                         <div className='flex text-center w-full border-y-[1px] border-[#fff] py-4'>
-                            <p className='font-poppins text-sm font-light text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui offici.</p>
+                            <p className='font-poppins text-sm font-extralight text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui offici.</p>
                         </div>
                         <div className='flex flex-col items-start justify-center w-full gap-4'>
                             <div className='flex gap-2 justify-evenly items-center w-full'>
@@ -44,7 +44,7 @@ export default function EventPage()
                                         alt='mask'
                                     />
                                 </div>
-                                <p className='font-poppins text-sm font-light text-white w-full'>Masks are obligatory for entrance</p>
+                                <p className='font-poppins text-sm font-extralight text-white w-full'>Masks are obligatory for entrance</p>
                             </div>
                             <div className='flex gap-2 justify-evenly items-center w-full'>
                                 <div className='w-1/6 flex items-center justify-center'>
@@ -55,7 +55,7 @@ export default function EventPage()
                                         alt='nochildren'
                                     />
                                 </div>
-                                <p className='font-poppins text-sm font-light text-white w-full'>No children under 16 years of age allowed, even at the availability of ticket.</p>
+                                <p className='font-poppins text-sm font-extralight text-white w-full'>No children under 16 years of age allowed, even at the availability of ticket.</p>
                             </div>
                         </div>
                     </div>
--- components/Homepage/EventsCarousel.tsx
+++ components/Homepage/EventsCarousel.tsx
@@ -58,7 +58,7 @@ export default function EventsCarousel()
                     <div className='flex justify-between items-center w-full'>
                         <p className='font-poppins font-medium text-2xl'>{selectedIndex === 0 ? 'Al Hilal VS Al Nassr' : 'Majid al-Muhandis'}</p>
                         <div className='flex flex-col gap-6 items-end'>
-                            <p className='font-poppins text-base font-light'>starting from {selectedIndex === 0 ? '2,900 EGP' : '3,200 EGP'}</p>
+                            <p className='font-poppins text-base font-extralight'>starting from {selectedIndex === 0 ? '2,900 EGP' : '3,200 EGP'}</p>
                             <button onClick={() => router.push(`/${selectedIndex === events.length - 1 ? '0' : `${selectedIndex + 1}`}`)} className='font-poppins text-[16px] bg-gradient-to-r from-[#E72377] from-[-5.87%] to-[#EB5E1B] to-[101.65%] w-fit px-3 py-2 text-white'>
                                 Book Now
                             </button>
@@ -66,12 +66,12 @@ export default function EventsCarousel()
                     </div>
                     <div className='flex gap-6 items-center justify-center flex-1 min-h-full'>
                         <div className='flex flex-col gap-2 justify-between items-end text-nowrap h-36'>
-                            <p className='font-poppins font-light text-lg'>Grand Cairo Stadium</p>
-                            <p className='font-poppins font-light text-lg'>May,28th,2024</p>
-                            <p className='font-poppins font-light text-lg'>Egypt,Cairo</p>
+                            <p className='font-poppins font-extralight text-lg'>Grand Cairo Stadium</p>
+                            <p className='font-poppins font-extralight text-lg'>May,28th,2024</p>
+                            <p className='font-poppins font-extralight text-lg'>Egypt,Cairo</p>
                         </div>
                         <div className='w-4 rotate-180 h-[172px] bg-[#7D40FF]' />
-                        <p className='font-poppins font-light text-base w-fit'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p className='font-poppins font-extralight text-base w-fit'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
             </section>
