666b1c390469e432d0cc45039b4d0ebaeae69839
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-02-23 21:25:25 +0200

    Event Page

--- constants/index.ts
+++ constants/index.ts
@@ -2,56 +2,56 @@ export const events = [
     {
         title: 'Majid al-Muhandis',
         price: '3,200 EGP',
-        imgUrl: '/assets/secondEvent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/secondEvent.jfif?alt=media&token=35b8fa9e-879b-4d9c-83f9-7833f4139446'
     },
     {
         title: 'Al Hilal VS Al Nassr',
         price: '2,900 EGP',
-        imgUrl: '/assets/firstevent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/firstevent.jfif?alt=media&token=f7960a45-6e80-4630-9153-39b4f4a776b0'
     },
     {
         title: 'Al Hilal VS Al Nassr',
         price: '2,900 EGP',
-        imgUrl: '/assets/thirdEvent.png'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/thirdEvent.png?alt=media&token=4bc8b98a-1f23-4399-b924-4527231e09d7'
     },
     {
         title: 'Al Hilal VS Al Nassr',
         price: '2,900 EGP',
-        imgUrl: '/assets/fourthEvent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/fourthEvent.jfif?alt=media&token=1d017a71-8cef-43f4-a0ad-1f7857a0e814'
     },
     {
         title: 'Al Hilal VS Al Nassr',
         price: '2,900 EGP',
-        imgUrl: '/assets/fourthEvent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/fourthEvent.jfif?alt=media&token=1d017a71-8cef-43f4-a0ad-1f7857a0e814'
     },
     {
         title: 'Amr Diab Tour 2024',
         price: '3,450 EGP',
-        imgUrl: '/assets/fifthEvent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/fifthevent.jfif?alt=media&token=14e43c69-0020-408f-a15e-fe04f43b53c2'
     },
     {
         title: 'Trevor Noah Tour 2024',
         price: '2,900 EGP',
-        imgUrl: '/assets/sixthEvent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/sixthevent.jfif?alt=media&token=c9d95d81-724f-4ccf-aaca-d68c4305313f'
     },
     {
         title: 'Justin Bieber Tour 2024',
         price: '2,900 EGP',
-        imgUrl: '/assets/seventhEvent.png'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/seventhevent.png?alt=media&token=483a186a-3d43-4dc4-810b-9640643caf46'
     },
     {
         title: 'Dubai World Cup 2024',
         price: '2,900 EGP',
-        imgUrl: '/assets/eighthEvent.png'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/eighthevent.png?alt=media&token=823057c1-58f0-4768-8ed2-709457cb6882'
     },
     {
         title: 'Black Coffee Culture Club',
         price: '2,900 EGP',
-        imgUrl: '/assets/ninthEvent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/ninthevent.jfif?alt=media&token=7e0a449f-0dd3-4fc8-a51a-b00a8db09ea2'
     },
     {
         title: 'The Phantom of The Opera',
         price: '2,900 EGP',
-        imgUrl: '/assets/tenthEvent.jfif'
+        imgUrl: 'https://firebasestorage.googleapis.com/v0/b/test-2cf5b.appspot.com/o/tenthevent.jfif?alt=media&token=62fcacd5-c34e-4b6b-b2e9-60512969362e'
     },
 ]
