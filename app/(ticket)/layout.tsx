076ccfdebfcaf27991c953dfb9d87f888e76ac65
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-03-12 15:28:00 +0200

    Tickets Pdf

---
+++ app/(ticket)/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { cookies } from "next/headers";
+import { decode } from "next-auth/jwt"
+import { UserType } from "@/lib/types/userTypes";
+import { redirect } from "next/navigation";
+import { initAdmin } from "@/firebase/server/config";
+import {  } from 'next/navigation'
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  preload: true,
+  adjustFontFallback: true,
+})
+
+export const metadata: Metadata = {
+  title: "Vibes",
+  description: "Buy and sell your tickets online",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const admin = await initAdmin()
+  const cookiesData = cookies()
+  const token = await decode({ token: cookiesData.get(process.env.NODE_ENV === 'production' ? '__Secure-next-auth.session-token' : 'next-auth.session-token')?.value, secret: process.env.NEXTAUTH_SECRET! })
+  if(token?.sub)
+  {
+    const user = (await admin.firestore().collection('users')?.doc(token?.sub as string).get()).data() as UserType
+    if(!user?.verified) return redirect('/complete-profile')
+  }
+
+  return (
+    <html lang="en">
+      <body className={cn('', poppins.variable)}>
+          <main className=''>
+              {children}
+          </main>
+      </body>
+    </html>
+  );
+}
