1325d0670a4d0c127f628b5fe3c207e6388cbb08
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-03-14 11:50:05 +0200

    Tickets Path Fix

--- app/api/sendMail/route.ts
+++ app/api/sendMail/route.ts
@@ -34,7 +34,7 @@ export async function POST(req: Request)
 
     await page.setContent(htmlString);
     await page.goto(process.env.NODE_ENV === 'production' ? `https://vibes-woad.vercel.app/ticket/${request.ticket.id}` : `http://localhost:3000/ticket/${request.ticket.id}`, {
-        waitUntil: 'networkidle2'
+        waitUntil: 'domcontentloaded'
     })
     await page.emulateMediaType('screen')
 
