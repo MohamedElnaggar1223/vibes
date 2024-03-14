aea15efd6f710edf4783894332224ea3b2e058e9
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-03-14 20:25:53 +0200

    Tickets Path Fix

--- app/api/sendMail/route.tsx
+++ app/api/sendMail/route.tsx
@@ -1,7 +1,6 @@
 import nodemailer from 'nodemailer'
 import chrome from '@sparticuz/chromium'
 import puppeteer from 'puppeteer-core'
-import { renderToPipeableStream } from 'react-dom/server'
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -24,7 +23,7 @@ export async function POST(req: Request)
         args: chrome.args,
         defaultViewport: chrome.defaultViewport,
         executablePath: await chrome.executablePath(),
-        headless: true,
+        headless: false,
     })
 
     const page = await browser.newPage()
@@ -32,7 +31,9 @@ export async function POST(req: Request)
     const htmlString = `<h1>This is a Ticket Pdf for ${request.event}</h1>`
 
     // await page.setContent(htmlString);
-    await page.goto(process.env.NODE_ENV === 'production' ? `https://vibes-woad.vercel.app/ticket/${request.ticket}` : `http://localhost:3000/ticket/${request.ticket}`)
+    await page.goto(process.env.NODE_ENV === 'production' ? `https://vibes-woad.vercel.app/ticket/${request.ticket}` : `http://localhost:3000/ticket/${request.ticket}`, {
+        waitUntil: 'domcontentloaded'
+    })
 
     // await page.waitForSelector('.bg-image')
     // await page.waitForSelector('.bg-image-inside')
--- components/shared/PurchaseTickets.tsx
+++ components/shared/PurchaseTickets.tsx
@@ -165,28 +165,15 @@ export default function PurchaseTickets({ event, exchangeRate, user }: Props)
             setPurchasedTickets(availableTickets.reduce((acc, ticket) => ({...acc, [ticket.name]: 0 }), {} as { [x: string]: number }))
             setPurchasedParkingPass(0)
             router.push(`/success/${addedTicket.id}`)
-            let maxRetries = 5
-            let attempts = 0
-            while(attempts < maxRetries)
-            {
-                attempts++
-                
-                try
-                {
-                    fetch(process.env.NODE_ENV === 'production' ? 'https://vibes-woad.vercel.app/api/sendMail' : 'http://localhost:3000/api/sendMail', {
-                        method: 'POST',
-                        body: JSON.stringify({
-                            "email": user?.email,
-                            "event": event.name,
-                            "ticket": addedTicket.id,
-                        })
-                    })
-                }
-                catch(e)
-                {
 
-                }
-            }
+            fetch(process.env.NODE_ENV === 'production' ? 'https://vibes-woad.vercel.app/api/sendMail' : 'http://localhost:3000/api/sendMail', {
+                method: 'POST',
+                body: JSON.stringify({
+                    "email": user?.email,
+                    "event": event.name,
+                    "ticket": addedTicket.id,
+                })
+            })
         }
         catch(e: any)
         {
