15fed5339ddaa091bb3326a63fd2ebab2c6d19de
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-03-16 17:55:30 +0200

    Test Tickets

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -8,6 +8,8 @@ import { formatTime, getDaySuffix } from "@/lib/utils"
 import { decode } from "next-auth/jwt"
 import { cookies } from "next/headers"
 import Image from "next/image"
+import { Suspense } from "react"
+import Loading from "./loading"
 
 type Props = {
     params: {
@@ -38,54 +40,56 @@ export default async function EventPage({ params }: Props)
     const user = token?.sub ? (await admin.firestore().collection('users')?.doc(token?.sub as string).get()).data() as UserType : null
 
     return (
-        <section className='flex flex-col w-full self-center gap-4 lg:h-[calc(100vh-7rem)] lg:max-h-[750px] lg:flex-row max-lg:items-center mt-16'>
-            <div className='flex flex-col w-full max-w-[390px] rounded-xl bg-[rgba(217,217,217,0.2)] gap-1 h-full'>
-                <ImageMotion 
-                    selectedEvent={selectedEvent}
-                    width={500}
-                    height={500}
-                    index={parseInt(params.id.toString())}
-                    imageClassName="rounded-t-xl"
-                    className="w-full object-contain h-full max-h-[212px] overflow-hidden"
-                    priority={true}
-                    layoutId={params.id.toString()}
-                    eventPage={true}
-                />
-                <div className='flex flex-col p-3 gap-4 flex-1'>
-                    <p className='font-poppins text-2xl font-bold text-white'>{selectedEvent?.name}</p>
-                    <div className='w-full flex justify-between items-center'>
-                        <p className='font-poppins text-md font-extralight text-white'>{selectedEvent?.venue}</p>
-                        <p className='font-poppins text-md font-extralight text-white mr-4'>{selectedEvent?.city}, {selectedEvent?.country}</p>
-                    </div>
-                    <p className='font-poppins text-md font-extralight text-white'>{`${months[selectedEvent.eventDate?.getMonth()]}, ${getDaySuffix(selectedEvent.eventDate?.getDate())}, ${selectedEvent.eventDate?.getFullYear()}`} | {formatTime(selectedEvent.eventTime)} {selectedEvent.timeZone}</p>
-                    <p className='font-poppins text-md font-extralight text-white'>{selectedEvent.gatesOpen && `Gates open ${formatTime(selectedEvent.gatesOpen)}`} {selectedEvent.gatesClose && `| Gates close ${formatTime(selectedEvent.gatesClose)}`}</p>
-                    <div className='flex text-center w-full border-y-[1px] border-[#fff] py-4'>
-                        <p className='font-poppins text-xs font-extralight text-white'>{selectedEvent.description}</p>
-                    </div>
-                    <div className='flex flex-col items-start justify-end w-full gap-6 mb-4 mt-auto flex-1'>
-                        {selectedEvent.eventDisclaimers.map(disclaimer => {
-                            const disclaimerText = disclaimer.icon.split('.')[0]
-                            const disclaimerSvg = disclaimerText === 'disclaimer1' ? '/assets/mask.svg' : '/assets/nochildren.svg'
-                            const width = disclaimerText === 'disclaimer1' ? 32 : 28
-                            const height = disclaimerText === 'disclaimer1' ? 20 : 28
-                            return (
-                                <div className='flex gap-2 justify-evenly items-center w-full'>
-                                    <div className='w-1/6 flex items-center justify-center'>
-                                        <Image
-                                            src={disclaimerSvg}
-                                            width={width}
-                                            height={height}
-                                            alt={disclaimerText}
-                                        />
+        <Suspense fallback={<Loading />}>
+            <section className='flex flex-col w-full self-center gap-4 lg:h-[calc(100vh-7rem)] lg:max-h-[750px] lg:flex-row max-lg:items-center mt-16'>
+                <div className='flex flex-col w-full max-w-[390px] rounded-xl bg-[rgba(217,217,217,0.2)] gap-1 h-full'>
+                    <ImageMotion 
+                        selectedEvent={selectedEvent}
+                        width={500}
+                        height={500}
+                        index={parseInt(params.id.toString())}
+                        imageClassName="rounded-t-xl"
+                        className="w-full object-contain h-full max-h-[212px] overflow-hidden"
+                        priority={true}
+                        layoutId={params.id.toString()}
+                        eventPage={true}
+                    />
+                    <div className='flex flex-col p-3 gap-4 flex-1'>
+                        <p className='font-poppins text-2xl font-bold text-white'>{selectedEvent?.name}</p>
+                        <div className='w-full flex justify-between items-center'>
+                            <p className='font-poppins text-md font-extralight text-white'>{selectedEvent?.venue}</p>
+                            <p className='font-poppins text-md font-extralight text-white mr-4'>{selectedEvent?.city}, {selectedEvent?.country}</p>
+                        </div>
+                        <p className='font-poppins text-md font-extralight text-white'>{`${months[selectedEvent.eventDate?.getMonth()]}, ${getDaySuffix(selectedEvent.eventDate?.getDate())}, ${selectedEvent.eventDate?.getFullYear()}`} | {formatTime(selectedEvent.eventTime)} {selectedEvent.timeZone}</p>
+                        <p className='font-poppins text-md font-extralight text-white'>{selectedEvent.gatesOpen && `Gates open ${formatTime(selectedEvent.gatesOpen)}`} {selectedEvent.gatesClose && `| Gates close ${formatTime(selectedEvent.gatesClose)}`}</p>
+                        <div className='flex text-center w-full border-y-[1px] border-[#fff] py-4'>
+                            <p className='font-poppins text-xs font-extralight text-white'>{selectedEvent.description}</p>
+                        </div>
+                        <div className='flex flex-col items-start justify-end w-full gap-6 mb-4 mt-auto flex-1'>
+                            {selectedEvent.eventDisclaimers.map(disclaimer => {
+                                const disclaimerText = disclaimer.icon.split('.')[0]
+                                const disclaimerSvg = disclaimerText === 'disclaimer1' ? '/assets/mask.svg' : '/assets/nochildren.svg'
+                                const width = disclaimerText === 'disclaimer1' ? 32 : 28
+                                const height = disclaimerText === 'disclaimer1' ? 20 : 28
+                                return (
+                                    <div className='flex gap-2 justify-evenly items-center w-full'>
+                                        <div className='w-1/6 flex items-center justify-center'>
+                                            <Image
+                                                src={disclaimerSvg}
+                                                width={width}
+                                                height={height}
+                                                alt={disclaimerText}
+                                            />
+                                        </div>
+                                        <p className='font-poppins text-xs font-extralight text-white w-full'>{disclaimer.disclaimer}</p>
                                     </div>
-                                    <p className='font-poppins text-xs font-extralight text-white w-full'>{disclaimer.disclaimer}</p>
-                                </div>
-                            )
-                        })}
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <PurchaseTickets exchangeRate={exchangeRate} event={selectedEvent} user={user} />
-        </section>
+                <PurchaseTickets exchangeRate={exchangeRate} event={selectedEvent} user={user} />
+            </section>
+        </Suspense>
     )
 }
--- components/shared/SearchBar.tsx
+++ components/shared/SearchBar.tsx
@@ -1,10 +1,18 @@
 'use client'
 import Image from "next/image";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function SearchBar() 
 {
     const [search, setSearch] = useState('')
+
+    const router = useRouter()
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(search) router.push(`/search?search=${search}`)
+    }
 
     return (
         <div className='w-full max-w-[627px] bg-white flex gap-4 rounded-md items-center justify-evenly px-4 mt-12'>
@@ -15,10 +23,17 @@ export default function SearchBar()
                 alt='search'
                 className='mr-[-5px] cursor-pointer' 
             />
-            <input 
+            <form                    
                 className='flex-1 my-2 p-0 border-x-[1px] border-[#E5E5E5] text-[10px] font-poppins pl-8 py-1.5 outline-none' 
-                placeholder='Search our events'
-            />
+                onSubmit={handleSubmit}
+            >
+                <input 
+                    placeholder='Search our events'
+                    className='flex-1 p-0 text-[10px] font-poppins outline-none' 
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                />
+            </form>
             <Image
                 src='/assets/settingsIcon.svg'
                 width={20}
