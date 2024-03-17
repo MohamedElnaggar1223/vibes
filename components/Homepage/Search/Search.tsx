c6a9f5b6ca20eaf181e0d9791f23f20c5974603f
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-03-17 12:08:44 +0200

    Search

--- components/Homepage/Search/Search.tsx
+++ components/Homepage/Search/Search.tsx
@@ -2,6 +2,8 @@ import { initAdmin } from "@/firebase/server/config"
 import { EventType } from "@/lib/types/eventTypes"
 import Image from "next/image"
 import Link from "next/link"
+import { Suspense } from "react"
+import SearchLoading from "./SearchLoading"
 
 type Props = {
     search: string
@@ -24,24 +26,26 @@ export default async function Search({ search }: Props)
     const events = await Promise.all(eventsDocs || [])
 
     return (
-        <section className='flex flex-col items-center justify-center w-full overflow-x-hidden flex-1 h-max'>
-            <div className='w-full flex justify-between items-center gap-8 flex-wrap mb-auto mt-12'>
-                {events.map(event => (
-                    <div key={event.id} className='min-w-48 min-h-48 rounded-lg overflow-hidden'>
-                        <Link
-                            href={`/events/${event.id}`}
-                        >
-                            <Image
-                                src={event?.eventPageImage}
-                                width={192} 
-                                height={192} 
-                                alt={event.name}
-                                className='object-cover min-w-48 min-h-48 cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out rounded-lg'
-                            />
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </section>
+        <Suspense fallback={<SearchLoading />}>
+            <section className='flex flex-col items-center justify-center w-full overflow-x-hidden flex-1 h-max'>
+                <div className='w-full flex justify-between items-center gap-8 flex-wrap mb-auto mt-12'>
+                    {events.map(event => (
+                        <div key={event.id} className='min-w-48 min-h-48 rounded-lg overflow-hidden'>
+                            <Link
+                                href={`/events/${event.id}`}
+                            >
+                                <Image
+                                    src={event?.eventPageImage}
+                                    width={192} 
+                                    height={192} 
+                                    alt={event.name}
+                                    className='object-cover min-w-48 min-h-48 cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out rounded-lg'
+                                />
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </Suspense>
     )
 }
