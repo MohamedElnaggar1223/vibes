cbf7e6e407bc36ce513319aa09efc6717e5dde45
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-03-19 13:41:27 +0200

    Filters

--- components/shared/SearchBar.tsx
+++ components/shared/SearchBar.tsx
@@ -94,7 +94,7 @@ export default function SearchBar()
                         </div>
                         <Separator />
                     </div>
-                    <div className='flex flex-col items-start justify-evenly w-[245px] bg-[#FAF9F9] gap-3 px-1.5 pt-2 pb-4'>
+                    <div className='flex flex-col items-start justify-evenly w-[257px] bg-[#FAF9F9] gap-3 px-1.5 pt-2 pb-4'>
                         <p className='font-poppins font-light text-black w-full text-center'>Choose Date</p>
                         <div className='flex gap-6 w-full items-center justify-between'>
                             <p onClick={() => setDate(todaysDate)} className={cn('font-poppins font-extralight cursor-pointer', date?.getDate() === todaysDate?.getDate() ? 'bg-[linear-gradient(90deg,rgba(231,35,119,1)50%,rgba(235,94,27,1)100%)] text-transparent bg-clip-text' : 'text-black')}>Today</p>
