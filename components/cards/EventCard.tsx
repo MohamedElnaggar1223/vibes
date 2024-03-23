f224d4664d58432d99f22f200048291c9c6e7eeb
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-03-23 13:23:35 +0200

    Mobile View

--- components/cards/EventCard.tsx
+++ components/cards/EventCard.tsx
@@ -25,7 +25,7 @@ export default async function EventCard({ event, exchangeRate }: Props)
                 layoutId={event.id}
                 eventPage={false}
             />
-            <div className='absolute flex z-[9999] bg-gradient-to-t from-black from-30% via-black/75 to-slate-900/25 w-[calc(100%-1rem)] h-full opacity-0 text-lg group-hover:opacity-100 bottom-0 right-[0%] duration-300 cursor-pointer'>
+            <div className='absolute max-lg:hidden flex z-[9999] bg-gradient-to-t from-black from-30% via-black/75 to-slate-900/25 w-[calc(100%-1rem)] h-full opacity-0 text-lg group-hover:opacity-100 bottom-0 right-[0%] duration-300 cursor-pointer'>
                 <Link href={`/events/${event.id}`} className='absolute w-full h-full'/>
                 <div className='flex flex-col gap-4 text-white mt-auto w-full h-fit pr-6 pb-3'>
                     <div className='flex flex-col justify-center items-start gap-2 h-full pl-2 border-l-[8px] border-[#E72377]'>
