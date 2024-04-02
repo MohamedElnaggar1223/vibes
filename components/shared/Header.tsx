a84d63d77d8618440078d8dc17aea0ae6e1297a5
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-04-02 11:21:28 +0200

    Lang fix

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -33,7 +33,7 @@ export default async function Header({ params }: Props)
                 {/* <Link href='/' className='text-white font-poppins text-lg font-[300]'>
                     Sell Your Tickets
                 </Link> */}
-                <Link href='/' locale={params.locale === 'en' ? 'ar' : 'en'} className='text-white font-poppins text-sm md:text-lg font-semibold'>
+                <Link replace={true} href='/' locale={params.locale === 'en' ? 'ar' : 'en'} className='text-white font-poppins text-sm md:text-lg font-semibold'>
                     {params.locale === 'en' ? 'AR' : 'EN' }
                 </Link>
                 {
