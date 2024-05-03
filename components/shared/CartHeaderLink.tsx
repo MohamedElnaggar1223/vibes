9892483fe39823fff73e4e1d35b5b1256d910d9a
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-05-03 10:52:25 +0300

    Cart Responsive and Arabic

--- components/shared/CartHeaderLink.tsx
+++ components/shared/CartHeaderLink.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { UserType } from "@/lib/types/userTypes";
-import { cn } from "@/lib/utils";
+import { cn, toArabicNums } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -21,7 +21,7 @@ export default function CartHeaderLink({ user }: Props)
         <Link onPointerEnter={() => setHovered(true)} onPointerLeave={() => setHovered(false)} href='/cart' className={cn('relative font-poppins text-sm cursor-pointer md:text-lg font-[300] z-[9999] text-white transition-all')}>
             {(user.cart?.tickets?.length ?? 0) > 0 && (
                 <div className='absolute -top-1 -right-1.5 w-2.5 h-2.5 rounded-full flex justify-center items-center'>
-                    <span className='text-white text-xs font-poppins font-normal'>{user.cart?.tickets.length}</span>
+                    <span className='text-white text-xs font-poppins font-normal'>{pathname?.startsWith('/ar') ? toArabicNums(`${user.cart?.tickets.length}`) : user.cart?.tickets.length}</span>
                 </div>
             )}
             {pathname?.includes('/cart') ? (
