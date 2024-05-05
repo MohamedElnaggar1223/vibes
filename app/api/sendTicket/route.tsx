7fabb5eeaffa3e58af0880f3f6cc49f52c61401e
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-05-05 12:44:36 +0300

    Mail Template

--- app/api/sendTicket/route.tsx
+++ app/api/sendTicket/route.tsx
@@ -22,7 +22,7 @@ export async function POST(req: Request)
     const request = await req.json()
     const { mailOptions, event, ticket } = request
 
-    console.log(event)
+    console.log(mailOptions.attachments[0].content)
 
     const eventData = {
         ...event,
