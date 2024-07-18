4dd5b9851486a7441019205267fbd2802865febd
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-07-18 21:30:04 +0300

    Callback

--- app/api/confirm-payment/route.ts
+++ app/api/confirm-payment/route.ts
@@ -1,12 +1,25 @@
 import { initAdmin } from "@/firebase/server/config"
 import { NextResponse } from "next/server"
 
-export async function GET(req: Request) {
-    const admin = await initAdmin()
+export async function POST(req: Request) {
+    try
+    {
+        const query = await req.json()
+    
+        const admin = await initAdmin()
+    
+        await admin.firestore().collection('payments').add({
+            ...query
+        })
+    }
+    catch(e)
+    {
+        
+    }
 
-    await admin.firestore().collection('payments').add({
-        ...req
-    })
+    return NextResponse.redirect('https://vibes-2yce-git-paymob-mohamedelnaggar1223s-projects.vercel.app/')
+}
 
+export async function GET() {
     return NextResponse.redirect('https://vibes-2yce-git-paymob-mohamedelnaggar1223s-projects.vercel.app/')
 }
