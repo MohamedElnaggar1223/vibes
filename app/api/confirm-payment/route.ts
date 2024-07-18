b22f15227ea9b3dcd9d788c73d9f98fd369442f2
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-07-18 20:59:44 +0300

    Callback

--- app/api/confirm-payment/route.ts
+++ app/api/confirm-payment/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server"
 export async function GET(req: Request) {
     console.log(req)
 
-    return NextResponse.redirect('/')
+    return NextResponse.redirect('https://vibes-2yce-git-paymob-mohamedelnaggar1223s-projects.vercel.app/')
 }
