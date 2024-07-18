8a2d4075749ef4955af3c2aaa96d1574c14e9a58
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-07-18 21:16:22 +0300

    Callback

--- app/api/confirm-payment/route.ts
+++ app/api/confirm-payment/route.ts
@@ -1,7 +1,12 @@
+import { initAdmin } from "@/firebase/server/config"
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
-    console.log(req)
+    const admin = await initAdmin()
+
+    admin.firestore().collection('payments').add({
+        ...req
+    })
 
     return NextResponse.redirect('https://vibes-2yce-git-paymob-mohamedelnaggar1223s-projects.vercel.app/')
 }
