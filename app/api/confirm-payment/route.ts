4f254b2ebf0ec2428fe18fce55c4df4a8a7c670c
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-07-19 16:21:38 +0300

    paymob

--- app/api/confirm-payment/route.ts
+++ app/api/confirm-payment/route.ts
@@ -1,19 +1,52 @@
 import { initAdmin } from "@/firebase/server/config"
 import { NextResponse } from "next/server"
+import { createHmac } from 'node:crypto'
 
 export async function POST(req: Request) {
     try
     {
-        const query = await req.json()
-        const params = req.url
-        const data = req.body
+        const { obj: query } = await req.json()
+        const hmac = req.url.replace('https://www.vibes-events.com/api/confirm-payment?hmac=', '')
+
+        const hmacRequiredFields = [
+            'amount_cents',
+            'created_at',
+            'currency',
+            'error_occured',
+            'has_parent_transaction',
+            'id',
+            'integration_id',
+            'is_3d_secure',
+            'is_auth',
+            'is_capture',
+            'is_refunded',
+            'is_standalone_payment',
+            'is_voided',
+            'order.id',
+            'owner',
+            'pending',
+            'source_data.pan',
+            'source_data.sub_type',
+            'source_data.type',
+            'success'
+        ]
+
+        let hmacData = ''
+
+        hmacRequiredFields.forEach(field => {
+            hmacData += query[field]
+        })
+
+        const hmacCalculated = createHmac('sha512', process.env.PAYMOB_HMAC!).update(hmacData).digest('hex')
+
+        if(hmacCalculated !== hmac) return NextResponse.json({ error: 'HMAC mismatch' }, { status: 400 })
     
         const admin = await initAdmin()
     
         await admin.firestore().collection('payments').add({
+            hmac,
+            hmacCalculated,
             ...query,
-            params,
-            ...data,
         })
     }
     catch(e)
